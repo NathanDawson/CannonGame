fed2fb946c5fe682b34e3aaf3594bcc935180e71
Nathan Dawson <nathan.dawson76@icloud.com>
2023-01-30 13:12:43 +0000

    Impliment TimeTracker class with Unit Tests

---
+++ Tech_Exc_Project_2/Tech_Exc_Project_2.UnitTests/TimeTracker.Tests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech_Exc_Project_2.UnitTests
+{
+    [TestClass]
+    public class TimeTrackerTests
+    {
+        [TestMethod]
+        public void TimeTracker_Given0_Return0()
+        {
+            var timeTracker = new TimeTracker();
+
+            timeTracker.StartTimer();
+
+            timeTracker.StopTimer();
+
+            Assert.AreEqual(timeTracker.GetTime(), 0);
+        }
+
+        [TestMethod]
+        public void TimeTracker_Given1_Return1()
+        {
+            var timeTracker = new TimeTracker();
+
+            timeTracker.StartTimer();
+
+            Thread.Sleep(1000);
+
+            timeTracker.StopTimer();
+
+            Assert.AreEqual(timeTracker.GetTime(), 1);
+        }
+
+        [TestMethod]
+        public void TimeTracker_Given2_Return2()
+        {
+            var timeTracker = new TimeTracker();
+
+            timeTracker.StartTimer();
+
+            Thread.Sleep(2000);
+
+            timeTracker.StopTimer();
+
+            Assert.AreEqual(timeTracker.GetTime(), 2);
+        }
+
+        [TestMethod]
+        public void TimeTracker_Given3_Return3()
+        {
+            var timeTracker = new TimeTracker();
+
+            timeTracker.StartTimer();
+
+            Thread.Sleep(3000);
+
+            timeTracker.StopTimer();
+
+            Assert.AreEqual(timeTracker.GetTime(), 3);
+        }
+
+        [TestMethod]
+        public void TimeTracker_Given10_Return10()
+        {
+            var timeTracker = new TimeTracker();
+
+            timeTracker.StartTimer();
+
+            Thread.Sleep(10000);
+
+            timeTracker.StopTimer();
+
+            Assert.AreEqual(timeTracker.GetTime(), 10);
+        }
+    }
+}
---
+++ Tech_Exc_Project_2/Tech_Exc_Project_2/TimeTracker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tech_Exc_Project_2;
+
+namespace Tech_Exc_Project_2
+{
+    public class TimeTracker : ITimeTracker
+    {
+        private int time;
+        private Stopwatch StopWatch;
+
+        public TimeTracker() 
+        {
+            this.time = 0;
+            StopWatch = new Stopwatch();
+        }
+
+        public void StartTimer()
+        {
+            StopWatch.Start();
+        }
+
+        public void StopTimer()
+        {
+            StopWatch.Stop();
+            time = (int)StopWatch.ElapsedMilliseconds / 1000;
+        }
+
+        public int GetTime()
+        {
+            return time;
+        }
+    }
+}
